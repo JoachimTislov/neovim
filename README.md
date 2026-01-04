ef0e70cc70087720b2dd09d8758c63dccb55d164
JoachimTislov <joachim.tislov@gmail.com>
2026-01-04 05:40:40 +0100

    Sync

--- README.md
+++ README.md
@@ -4,23 +4,20 @@ Compatible with Neovim 0.12 and above. Works on hopyefully all OSes that Neovim 
 
 ## TODOs
 
-- Use native doc view, not inline by blink.cmp or whatever it is.
-    - `vim.diagnostics.opts.float`?
-- Add "f" - find, in combination with operators ( d, c, y ) to "do" until specified char.
-- setup proper diagnostics workflow
-    - add method in on_attach to load all files and thereby get all diagnostics
-    - https://github.com/artemave/workspace-diagnostics.nvim
-- move between error/warning/hint/info (keybind)
-- preview to mini.picker
-- custom config support
-- look at old config for forgotten features
-- mini, snacks, github prs manager
-- explore treesitter built upon features
-- web icons
+- mini, folke plugins (flash, snacks), github prs manager
+    - mini statusline?
 - fzf, cmp (cmp-nvim-lsp?)
 - fininsh(/add more) snippets
+- consider replacing conform with custom setup (linting, formatting, etc)
+- replace blink.cmp with https://github.com/hrsh7th/nvim-cmp?
+- look into https://github.com/mbbill/undotree?tab=readme-ov-file
+- consider https://github.com/nvim-pack/nvim-spectre
 
 ## Windows specific features
 
 - Syncs wsl clipboard with system clipboard
     - but not powertoys clipboard manager
+
+### TODO
+
+- Add navigation between Wezterm and nvim (navigator - https://github.com/numToStr/Navigator.nvim)
--- init.lua
+++ init.lua
@@ -24,49 +24,74 @@ vim.opt.listchars = { tab = '▸ ', trail = '·', nbsp = '␣' }
 vim.o.updatetime = 250
 -- problematic with some plugins
 -- vim.opt.autochdir = true
+vim.o.chistory = 100
+vim.o.lhistory = 100
 
+-- Base url for github isn't abstracted away to allow 'gx'
+-- e.g., local url_github = ...
 vim.pack.add {
   -- Dependencies of many plugins
-  'https://github.com/folke/lazydev.nvim', -- Helper for developing plugins
   'https://github.com/nvim-lua/plenary.nvim', -- Storage of complete lua functions
   'https://github.com/nvim-mini/mini.icons',
 
   -- Plugins
-  'https://github.com/stevearc/oil.nvim',
-  'https://github.com/stevearc/conform.nvim',
-  'https://github.com/nvim-mini/mini.pick',
-  'https://github.com/neovim/nvim-lspconfig',
-  'https://github.com/williamboman/mason.nvim',
-  'https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim',
-  'https://github.com/NeogitOrg/neogit',
-  'https://github.com/sindrets/diffview.nvim',
-  'https://github.com/L3MON4D3/LuaSnip',
-  'https://github.com/Saghen/blink.cmp',
-  'https://github.com/folke/which-key.nvim',
-  'https://github.com/CopilotC-Nvim/CopilotChat.nvim',
-  'https://github.com/rose-pine/neovim',
-  'https://github.com/MeanderingProgrammer/render-markdown.nvim',
-  'https://github.com/windwp/nvim-autopairs',
-  'https://github.com/windwp/nvim-ts-autotag',
-  'https://github.com/nvim-treesitter/nvim-treesitter',
-  'https://github.com/catgoose/nvim-colorizer.lua',
-  'https://github.com/github/copilot.vim',
-  'https://github.com/lewis6991/gitsigns.nvim',
-  'https://github.com/artemave/workspace-diagnostics.nvim',
-  'https://github.com/sahilsehwag/macrobank.nvim',
+  'https://github.com/stevearc/oil.nvim', -- File explorer
+  'https://github.com/stevearc/conform.nvim', -- Code formatter
+  'https://github.com/nvim-mini/mini.pick', -- Fuzzy picker
+  'https://github.com/neovim/nvim-lspconfig', -- LSP configurations
+  'https://github.com/williamboman/mason.nvim', -- Installer UI
+  'https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim', -- Auto install packages
+  'https://github.com/lewis6991/gitsigns.nvim', -- Git signs in signcolumn
+  'https://github.com/NeogitOrg/neogit', -- Git interface
+  'https://github.com/sindrets/diffview.nvim', -- Git diff viewer
+  'https://github.com/L3MON4D3/LuaSnip', -- Snippet engine
+  'https://github.com/Saghen/blink.cmp', -- Fuzzy completion source
+  'https://github.com/folke/which-key.nvim', -- Keybinding helper
+  'https://github.com/github/copilot.vim', -- GitHub Copilot
+  'https://github.com/CopilotC-Nvim/CopilotChat.nvim', -- AI chat assistant
+  'https://github.com/rose-pine/neovim', -- Color scheme
+  'https://github.com/MeanderingProgrammer/render-markdown.nvim', -- Markdown renderer
+  'https://github.com/windwp/nvim-autopairs', -- Create pairs like (), {}, []
+  'https://github.com/windwp/nvim-ts-autotag', -- Auto close and rename html tags
+  'https://github.com/nvim-treesitter/nvim-treesitter', -- Treesitter configurations
+  'https://github.com/kylechui/nvim-surround', -- Surround text objects
+  'https://github.com/catgoose/nvim-colorizer.lua', -- Color highlighter
+  'https://github.com/christoomey/vim-tmux-navigator', -- Tmux navigation
+  'https://github.com/mfussenegger/nvim-dap', -- Debug Adapter Protocol client
+  'https://github.com/igorlfs/nvim-dap-view', -- Minimal DAP UI TODO: Test this
+  'https://github.com/rcarriga/nvim-dap-ui', -- UI for nvim-dap
+  'https://github.com/nvim-neotest/nvim-nio', -- dependency of nvim-dap-ui
+  'https://github.com/leoluz/nvim-dap-go', -- Go adapter
+
+  -- not important but can be nice to have
+  'https://github.com/artemave/workspace-diagnostics.nvim', -- Loads diagnostics for all files in workspace
+  'https://github.com/j-hui/fidget.nvim', -- LSP status
+  'https://github.com/meznaric/key-analyzer.nvim', -- Analyze your keymaps
+  'https://github.com/NMAC427/guess-indent.nvim', -- Guess indentation settings
 }
 require('blink.cmp').setup {
   fuzzy = { implementation = 'lua' },
 }
+require('fidget').setup()
+require('nvim-surround').setup {}
 require('mini.icons').setup()
-require('macrobank').setup()
+require('guess-indent').setup {}
 require('colorizer').setup {
   user_default_options = {
     names = false,
     css = true,
   },
 }
-require('workspace-diagnostics').setup()
+-- TODO: Test these two ...
+-- require('lspconfig').lua_ls.setup {
+--   on_attach = function(client, bufnr)
+--     require('workspace-diagnostics').populate_workspace_diagnostics(client, bufnr)
+--   end,
+-- }
+-- Future alternative to workspace-diagnostics.nvim ?
+-- vim.lsp.config.ts_ls.on_attach = function(client, bufnr)
+--   require('workspace-diagnostics').populate_workspace_diagnostics(client, bufnr)
+-- end
 require('nvim-treesitter.configs').setup {
   ensure_installed = { 'markdown', 'lua', 'svelte', 'css', 'html', 'typescript', 'java' },
   auto_install = true,
@@ -150,8 +175,15 @@ vim.schedule(function()
   vim.o.clipboard = 'unnamedplus'
 end)
 
+require('key-analyzer').setup()
+nmap('<leader>ok', ':KeyAnalyzer ', { desc = '[O]pen KeyAnalyzer' })
+
 local pick = require 'mini.pick'
-pick.setup {}
+pick.setup {
+  mappings = {
+    toggle_preview = '<Space>',
+  },
+}
 
 --- [P]ick [P]roject ---
 nmap('<leader>pp', function()
@@ -282,7 +314,9 @@ require('which-key').setup {
     { '<leader>q', group = '[Q]uit' },
     { '<leader>p', group = '[P]ick' },
     { '<leader>o', group = '[O]pen' },
+    { '<leader>d', group = '[D]ebug' },
     { '<leader>l', group = '[L]ist' },
+    { '<leader>t', group = '[T]oggle' },
     { '<leader>c', group = '[C]opilot' },
     { '<leader>g', group = '[G]it Hunk', mode = { 'n', 'v' } },
     { '<leader>gr', group = 'Lsp requests' },
@@ -411,22 +445,26 @@ nmap('<leader>qf', '<cmd>q!<cr>', { desc = '[Q]uit [F]orce' })
 
 -- [O]pen
 nmap('<leader>od', '<cmd>DiffviewOpen<cr>', { desc = '[O]pen [D]iffview' })
-nmap('<leader>om', '<cmd>Mason<cr>', { desc = '[O]pen [M]ason' })
+nmap('<leader>oM', '<cmd>Mason<cr>', { desc = '[O]pen [M]ason' })
 nmap('<leader>on', '<cmd>Neogit<cr>', { desc = '[O]pen [N]eogit' })
 nmap('<leader>os', '<cmd>split<cr>', { desc = '[O]pen [S]plit' })
 nmap('<leader>ov', '<cmd>vsplit<cr>', { desc = '[O]pen [V]ertical split' })
 nmap('<leader>oc', '<cmd>e $MYVIMRC<cr>', { desc = '[O]pen [C]onfig' })
-
--- [L]ist
-nmap('<leader>lq', '<cmd>copen<cr>', { desc = '[L]ist [Q]uickfix' })
-nmap('<leader>lm', '<cmd>messages<cr>', { desc = '[L]ist [M]essages' })
-nmap('<leader>lc', '<cmd>chistory<cr>', { desc = '[L]ist [C]History' })
-nmap('<leader>ll', '<cmd>lhistory<cr>', { desc = '[L]ist [L]History' })
+nmap('<leader>om', '<cmd>messages<cr>', { desc = '[O]pen [M]essages' })
+nmap('<leader>oq', '<cmd>copen<cr>', { desc = '[O]pen [Q]uickfix list' })
+nvmap('<leader>oh', function()
+  local word = vim.fn.expand '<cword>'
+  print('checking help for ' .. word)
+  local ok = pcall(vim.cmd, 'help ' .. word)
+  if not ok then
+    vim.notify('No manual entry for ' .. word, vim.log.levels.WARN)
+  end
+end, { desc = '[O]pen [H]elp for selected/current word' })
 
 -- [C]opilot
 local copilot_chat = require 'CopilotChat'
 nmap('<leader>cp', copilot_chat.select_prompt, { desc = 'View/select [p]rompt templates' })
-nmap('<leader>ct', copilot_chat.toggle, { desc = '[T]oggle chat window' })
+nvmap('<leader>ct', copilot_chat.toggle, { desc = '[T]oggle chat window' })
 nmap('<leader>cs', copilot_chat.stop, { desc = '[S]top current output' })
 nmap('<leader>cr', copilot_chat.reset, { desc = '[R]eset chat' })
 nmap('<leader>cm', copilot_chat.select_model, { desc = 'View/select available models' })
@@ -446,16 +484,12 @@ nmap('<leader>pc', function()
 end, { desc = '[P]ick [C]ommand' })
 
 -- Actions
+vmap('<BS>', 'y/<c-r>"<cr>', { desc = 'Search with selected text' })
+nmap('<BS>', '/', { desc = 'Search' })
 nmap('<s-h>', '<cmd>Oil<cr>', { desc = 'Oil (File browser)' })
 nmap('<leader>e', '<cmd>q<cr>', { desc = '[E]xit' })
 nmap('<leader>w', '<cmd>w<cr>', { desc = 'Write to file' })
 nmap('<leader>s', ':source<cr>')
-
--- Quickfix list navigation
--- nmap('<M-j>', '<cmd>cnext<cr>')
--- nmap('<M-k>', '<cmd>cprev<cr>')
--- nmap('<M-h>', '<cmd>cfirst<cr>')
--- nmap('<M-l>', '<cmd>clast<cr>')
 
 -- Copilot
 nmap('<leader>ce', '<cmd>Copilot enable<cr>', { desc = 'Enable' })
@@ -469,6 +503,12 @@ nvmap('<C-j>', '<C-W>j')
 nvmap('<C-k>', '<C-W>k')
 nvmap('<C-l>', '<C-W>l')
 nvmap('<C-h>', '<C-W>h')
+
+-- If tmux is running, use the following mappings to navigate between tmux panes and nvim splits seamlessly
+nvmap('<C-h>', '<cmd>TmuxNavigateLeft<cr>')
+nvmap('<C-l>', '<cmd>TmuxNavigateRight<cr>')
+nvmap('<C-j>', '<cmd>TmuxNavigateDown<cr>')
+nvmap('<C-k>', '<cmd>TmuxNavigateUp<cr>')
 
 -- Center editor view with zz
 nvmap('<C-d>', '<C-d>zz')
@@ -506,9 +546,109 @@ vim.api.nvim_create_autocmd('LspAttach', {
   end,
 })
 
+---------------------------------------------
+--- Debug Adapter Protocol configurations ---
+---------------------------------------------
+
+local dap = require 'dap'
+local dapui = require 'dapui'
+local widgets = require 'dap.ui.widgets'
+
+nmap('<leader>dh', widgets.hover, { desc = 'Debug: Hover' })
+nmap('<leader>dp', widgets.preview, { desc = 'Debug: Preview' })
+nmap('<leader>df', function()
+  widgets.centered_float(widgets.frames)
+end, { desc = 'Debug: View Frames' })
+nmap('<leader>ds', function()
+  widgets.centered_float(widgets.scopes)
+end, { desc = 'Debug: View Scopes' })
+-- restart and run_last are basically the same, but run_last is more usable
+nmap('<leader>dr', dap.run_last, { desc = 'Debug: Run last' })
+-- nmap('<leader>dr', dap.restart, { desc = 'Debug: Restart'})
+nmap('<leader>dc', dap.continue, { desc = 'Debug: Continue' })
+nmap('<leader>dd', dap.disconnect, { desc = 'Debug: Disconnect' })
+nmap('<leader>dx', dap.terminate, { desc = 'Debug: Terminate' })
+nmap('<leader>dp', dap.pause, { desc = 'Debug: Pause' })
+nmap('<leader>di', dap.step_into, { desc = 'Debug: Step into' })
+nmap('<leader>do', dap.step_over, { desc = 'Debug: Step over' })
+nmap('<leader>de', dap.step_out, { desc = 'Debug: Step out' })
+nmap('<leader>du', dap.step_back, { desc = 'Debug: Step back' })
+nmap('<leader>db', dap.toggle_breakpoint, { desc = 'Debug: Toggle breakpoint' })
+nmap('<leader>dR', dap.clear_breakpoints, { desc = 'Debug: Clear breakpoints' })
+nmap('<leader>dB', function()
+  dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
+end, { desc = 'Debug: Set Breakpoint' })
+-- TODO: enable these once I start using Go tests. dt conflicts with restart
+-- Need better keybind
+-- nmap('<leader>dt', require('dap-go').debug_test, { desc = 'Debug: test'})
+-- nmap('<leader>dl', require('dap-go').dapgo.debug_last_test, { desc = 'Debug: Last test' })
+
+-- For more information, see |:help nvim-dap-ui|
+dapui.setup {
+  icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
+  controls = {
+    icons = {
+      pause = '⏸',
+      play = '▶',
+      step_into = '⏎',
+      step_over = '⏭',
+      step_out = '⏮',
+      step_back = 'b',
+      run_last = '▶▶',
+      terminate = '⏹',
+      disconnect = '⏏',
+    },
+  },
+}
+
+dap.configurations.lua = {
+  {
+    type = 'nlua',
+    request = 'attach',
+    name = 'Attach to running Neovim instance',
+  },
+}
+
+dap.adapters.nlua = function(callback, config)
+  callback { type = 'server', host = config.host or '127.0.0.1', port = config.port or 8086 }
+end
+
+-- Change breakpoint icons
+-- vim.api.nvim_set_hl(0, 'DapBreak', { fg = '#e51400' })
+-- vim.api.nvim_set_hl(0, 'DapStop', { fg = '#ffcc00' })
+-- local breakpoint_icons = vim.g.have_nerd_font
+--     and { Breakpoint = '', BreakpointCondition = '', BreakpointRejected = '', LogPoint = '', Stopped = '' }
+--   or { Breakpoint = '●', BreakpointCondition = '⊜', BreakpointRejected = '⊘', LogPoint = '◆', Stopped = '⭔' }
+-- for type, icon in pairs(breakpoint_icons) do
+--   local tp = 'Dap' .. type
+--   local hl = (type == 'Stopped') and 'DapStop' or 'DapBreak'
+--   vim.fn.sign_define(tp, { text = icon, texthl = hl, numhl = hl })
+-- end
+
+dap.listeners.after.event_initialized['dapui_config'] = dapui.open
+dap.listeners.before.event_terminated['dapui_config'] = dapui.close
+dap.listeners.before.event_exited['dapui_config'] = dapui.close
+
+require('dap-go').setup {
+  dap_configurations = {
+    {
+      type = 'go',
+      name = 'Debug Main',
+      request = 'launch',
+      program = 'main.go',
+    },
+  },
+  delve = {},
+}
+
 --------------------------------------
 --- Quality of life configurations ---
 --------------------------------------
+
+-- insert with indentation on empty lines
+nmap('i', function()
+  return string.match(vim.api.nvim_get_current_line(), '%g') == nil and 'cc' or 'i'
+end, { expr = true, noremap = true })
 
 --- wsl, forward copy to windows clipboard ---
 --- Note: It does not update the powertools clipboard
@@ -637,10 +777,7 @@ vim.lsp.handlers['textDocument/publishDiagnostics'] = function(err, result, ctx)
   vim.lsp.diagnostic.on_publish_diagnostics(err, result, ctx)
 end
 
----------------------------------------------------------
 --- prevent auto inserting comment prefix on new line ---
----------------------------------------------------------
-
 autocmd('FileType', {
   group = vim.api.nvim_create_augroup('format-options', { clear = true }),
   callback = function()
@@ -648,10 +785,7 @@ autocmd('FileType', {
   end,
 })
 
-----------------------------------------------------
 --- restore cursor position when reopening files ---
-----------------------------------------------------
-
 autocmd('BufReadPost', {
   group = vim.api.nvim_create_augroup('restore-cursor', { clear = true }),
   callback = function(args)
@@ -668,20 +802,7 @@ autocmd('BufReadPost', {
   end,
 })
 
--------------------------------------------------------
--- open help files in a vertical split on the right ---
--------------------------------------------------------
-
-autocmd('FileType', {
-  group = vim.api.nvim_create_augroup('help-right', { clear = true }),
-  pattern = 'help',
-  command = 'wincmd L',
-})
-
--------------------------------------------------------
 -- equalize window sizes when resizing Neovim window ---
--------------------------------------------------------
-
 autocmd('VimResized', {
   group = vim.api.nvim_create_augroup('resize-windows', { clear = true }),
   command = 'wincmd =',
